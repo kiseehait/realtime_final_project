f3e75c974d41cc2c6b7988060729984d0081cd47
Seehait Chockthanyawat <kiseehait@gmail.com>
2015-12-12 18:44:09 +0700

    Read object from file.

--- WebGL/js/basic.js
+++ WebGL/js/basic.js
@@ -72,6 +72,89 @@ var faces = [
 	face3(1,2,8,3,13,8,4,14,8)
 ]
 
+function readObjectFromFile(file) {
+	var geometry = new THREE.Geometry();
+
+	var oFile = new XMLHttpRequest();
+	oFile.open("GET", file, false);
+
+	var allText = "";
+
+	oFile.onreadystatechange = function ()
+	{
+		if(oFile.readyState === 4)
+		{
+			if(oFile.status === 200 || oFile.status == 0)
+			{
+				allText = oFile.responseText;
+				// alert(allText);
+			}
+		}
+	}
+	oFile.send(null);
+
+	var lines = allText.split('\n');
+	for(var line = 0; line < lines.length; line++) {
+		// console.log(lines[line]);
+		switch (lines[line].substring(0, 2)) {
+			case "v ":
+				var vstring = lines[line].split(' ');
+				vertices.push(
+					new THREE.Vector3( parseFloat(vstring[1]), parseFloat(vstring[2]), parseFloat(vstring[3]) )
+				);
+
+				// for debugging
+				console.log("v " + parseFloat(vstring[1]) + " " + parseFloat(vstring[2]) + " " + parseFloat(vstring[3]));
+
+				break;
+			case "vt":
+				var uvstring = lines[line].split(' ');
+				uvs.push(
+					new THREE.Vector3( parseFloat(uvstring[1]), parseFloat(uvstring[2]) )
+				);
+
+				// for debugging
+				console.log("vt " + parseFloat(uvstring[1]) + " " + parseFloat(uvstring[2]));
+
+				break;
+			case "vn":
+				var nstring = lines[line].split(' ');
+				normals.push(
+					new THREE.Vector3( parseFloat(nstring[1]), parseFloat(nstring[2]), parseFloat(nstring[3]) )
+				);
+
+				// for debugging
+				console.log("n " + parseFloat(nstring[1]) + " " + parseFloat(nstring[2]) + " " + parseFloat(nstring[3]));
+
+				break;
+			case "f ":
+				var fstring = lines[line].split(' ');
+				var fs0 = fstring[1].split('/');
+				var fs1 = fstring[2].split('/');
+				var fs2 = fstring[3].split('/');
+
+				faces.push(
+					face3(parseInt(fs0[0]), parseInt(fs0[1]), parseInt(fs0[2]), 
+						parseInt(fs1[0]), parseInt(fs1[1]), parseInt(fs1[2]), 
+						parseInt(fs2[0]), parseInt(fs2[1]), parseInt(fs2[2]))
+				);
+
+				// for debugging
+				console.log("f " + parseInt(fs0[0]) + "/" + parseInt(fs0[1]) + "/" + parseInt(fs0[2]) + " " + 
+					parseInt(fs1[0]) + "/" + parseInt(fs1[1]) + "/" + parseInt(fs1[2]) + " " + 
+					parseInt(fs2[0]) + "/" + parseInt(fs2[1]) + "/" + parseInt(fs2[2]));
+
+				break;
+			case "us":
+				var mstring = lines[line].split(' ');
+
+				// for debugging
+				console.log("material " + mstring[1]);
+		}
+	}
+}
+
+readObjectFromFile('./js/tree.obj');
 
 var geometry2 = new THREE.Geometry();
 
