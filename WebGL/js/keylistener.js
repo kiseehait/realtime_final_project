2a8f8a2188af9c0b8bffc0ac9f5cef91cf4ba0db
Seehait Chockthanyawat <kiseehait@gmail.com>
2015-12-12 21:33:57 +0700

    Update key listener
    Now support ‘Thai’ keyboard interface.

--- WebGL/js/keylistener.js
+++ WebGL/js/keylistener.js
@@ -3,21 +3,27 @@ $(function() {
 		var key = e.which;
 		console.log("You pressing: " + key);
 		switch (key) {
+			case 3615 :
 			case 97 :
 				camera.position.x++;
 				break;
+			case 3612 :
 			case 122 :
 				camera.position.x--;
 				break;
+			case 3627 :
 			case 115 :
 				camera.position.y++;
 				break;
+			case 3611 :
 			case 120 :
 				camera.position.y--;
 				break;
+			case 3585 :
 			case 100 :
 				camera.position.z++;
 				break;
+			case 3649 :
 			case 99 :
 				camera.position.z--;
 				break;
