c90ee5e8e9c8b7da47b4b1a80d258e90f6c9e114
Mongkud Klungpornkun <mongkud167@gmail.com>
2015-12-12 20:21:55 +0700

    Update Shader

--- WebGL/js/basic.js
+++ WebGL/js/basic.js
@@ -5,19 +5,31 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var uniforms = {
+	fogDensity: { type: "f", value: 0.45 },
+	lightPos: { type: "v3", value: new THREE.Vector3( 0, 100, 200 ) },
+	lightCol: { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
+	Ka: { type: "v3", value: new THREE.Vector3( 0.1, 0.1, 0.1 ) },
+	Kd: { type: "v3", value: new THREE.Vector3( 1, 0, 0 ) },
+	Ks: { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
+	s: { type: "f", value: 100 }
+};
+
 var geometry = readObjectFromFile('tree.obj');;
-var material = new THREE.MeshBasicMaterial( { color: 0xaaffaa } );
-var cube = new THREE.Mesh( geometry, material );
+var material = new THREE.ShaderMaterial( {
+	uniforms: uniforms,
+	vertexShader: document.getElementById( 'vertexShader' ).textContent,
+	fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+} );
+var tree = new THREE.Mesh( geometry, material );
 
-scene.add( cube );
-
-camera.position.z = 30;
+scene.add( tree );
+tree.position.y = -8;
+camera.position.z = 12;
 
 var render = function () {
 	requestAnimationFrame( render );
-
-	cube.rotation.y += 0.01;
-
+	tree.rotation.y += 0.01;
 	renderer.render(scene, camera);
 };
 
