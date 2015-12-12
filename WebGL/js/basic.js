d3fa4589680090d96241127bf93cb39115804978
Mongkud Klungpornkun <mongkud167@gmail.com>
2015-12-12 21:11:59 +0700

    Update textures

--- WebGL/js/basic.js
+++ WebGL/js/basic.js
@@ -5,31 +5,39 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var mytexture = THREE.ImageUtils.loadTexture( "./textures/box.jpg" );
+/*
+mytexture.wrapS = THREE.RepeatWrapping;
+mytexture.wrapT = THREE.RepeatWrapping;
+mytexture.repeat.set( 4, 4 );
+*/
 var uniforms = {
 	fogDensity: { type: "f", value: 0.45 },
 	lightPos: { type: "v3", value: new THREE.Vector3( 0, 100, 200 ) },
 	lightCol: { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
 	Ka: { type: "v3", value: new THREE.Vector3( 0.1, 0.1, 0.1 ) },
-	Kd: { type: "v3", value: new THREE.Vector3( 1, 0, 0 ) },
+	Kd: { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
 	Ks: { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
-	s: { type: "f", value: 100 }
+	s: { type: "f", value: 100 },
+	texture: { type: "t", value: mytexture}
 };
 
-var geometry = readObjectFromFile('tree.obj');;
+var geometry = readObjectFromFile('cube.obj');;
 var material = new THREE.ShaderMaterial( {
 	uniforms: uniforms,
 	vertexShader: document.getElementById( 'vertexShader' ).textContent,
 	fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 } );
-var tree = new THREE.Mesh( geometry, material );
+var object = new THREE.Mesh( geometry, material );
 
-scene.add( tree );
-tree.position.y = -8;
-camera.position.z = 12;
+scene.add( object );
+object.position.y = 0;
+camera.position.z = 5;
 
 var render = function () {
 	requestAnimationFrame( render );
-	tree.rotation.y += 0.01;
+	object.rotation.y += 0.01;
+	object.rotation.x += 0.01;
 	renderer.render(scene, camera);
 };
 
