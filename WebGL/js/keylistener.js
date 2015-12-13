56f3c6a855aed56af43e8e7fe676e9a2c1c38941
Panusorn Wonganankit <apanusorna@hotmail.com>
2015-12-13 12:39:39 +0700

    New OOP Version, Minor  Update from "Master"

--- WebGL/js/keylistener.js
+++ WebGL/js/keylistener.js
@@ -1,62 +1,100 @@
 var lastTimeUpdate = Date.now();
 var _CAMERA_SPEED = 0.01;
+var _PAN_SPEED = 0.01;
 var _INVALIDKEYS = {"9":"tab","18":"alt"};
-var _UP    = "87"; // W
-var _DOWN  = "83"; // S
+var _UP    = "82"; // R
+var _DOWN  = "70"; // F
 var _LEFT  = "65"; // A
 var _RIGHT = "68"; // D
-var _FRONT = "69"; // E
-var _BACK  = "81"; // Q
+var _FRONT = "87"; // W
+var _BACK  = "83"; // S
 
-// A map to remember in
+var verticalAngle = 0,horizontalAngle = Math.PI;
+
+var cursorX,cursorY,newCursorX,newCursorY;
+var cursorDrag = false;
+var position = new THREE.Vector3(0, 1.5, 0);
+var direction = new THREE.Vector3( Math.cos(verticalAngle) * Math.sin(horizontalAngle), Math.sin(verticalAngle), Math.cos(verticalAngle) * Math.cos(horizontalAngle) );
+var right = new THREE.Vector3( Math.sin(horizontalAngle - Math.PI/2), 0, Math.cos(horizontalAngle - Math.PI/2) );
+var up = new THREE.Vector3();
+up.crossVectors(right, direction);
+
 var keysdown = {};
+var delta = function() {
+	return Date.now()-lastTimeUpdate;
+}
+var crop = function(n, min, max) {
+	if(n < min) return min;
+	if(n > max) return max;
+	return n;
+}
 
-// keydown handler
+// Cursor Handler
+document.onmousedown = function(e) {
+	cursorDrag = true;
+}
+document.onmouseup = function(e) {
+	cursorDrag = false;
+}
+document.onmousemove = function(e){
+	cursorX = newCursorX;
+	cursorY = newCursorY;
+	newCursorX = e.pageX;
+	newCursorY = e.pageY;
+}
+
+// Keyboard Handler
 $(document).keydown(function(e){
-
-  // Do we already know it's down?
-  if (keysdown[e.keyCode]) {
-      // Ignore it
+  if (keysdown[e.keyCode])
       return;
-  }
-
-  // Remember it's down
 	if(!(e.keyCode in _INVALIDKEYS))
 		keysdown[e.keyCode] = true;
 });
-
-// keyup handler
 $(document).keyup(function(e){
   delete keysdown[e.keyCode];
 });
 
-var delta = function() {
-	return Date.now()-lastTimeUpdate;
-}
 
 function updateCamera() {
+	direction.set( Math.cos(verticalAngle) * Math.sin(horizontalAngle), Math.sin(verticalAngle), Math.cos(verticalAngle) * Math.cos(horizontalAngle) );
+	right.set( Math.sin(horizontalAngle - Math.PI/2), 0, Math.cos(horizontalAngle - Math.PI/2) );
+	up.crossVectors(right, direction);
+	if(cursorDrag) {
+		horizontalAngle += _PAN_SPEED * (newCursorX - cursorX);
+		verticalAngle   = crop(verticalAngle + _PAN_SPEED * (newCursorY - cursorY), -Math.PI/2+0.1, Math.PI/2-0.1);
+		console.log(horizontalAngle + " , " + verticalAngle);
+		camera.lookAt(new THREE.Vector3(
+			position.x + Math.cos(verticalAngle) * Math.sin(horizontalAngle),
+			position.y + Math.sin(verticalAngle),
+			position.z + Math.cos(verticalAngle) * Math.cos(horizontalAngle)
+		));
+	}
+	cursorX = newCursorX;
+	cursorY = newCursorY;
 	for(var key in keysdown) {
-		console.log(key);
 		switch (key) {
-			case _LEFT:
-				camera.position.x -= delta()*_CAMERA_SPEED;
-				break;
-			case _RIGHT:
-				camera.position.x += delta()*_CAMERA_SPEED;
-				break;
-			case _UP:
-				camera.position.y += delta()*_CAMERA_SPEED;
-				break;
-			case _DOWN:
-				camera.position.y -= delta()*_CAMERA_SPEED;
-				break;
 			case _FRONT:
-				camera.position.z -= delta()*_CAMERA_SPEED;
+				position.add((new THREE.Vector3()).addScaledVector(direction,delta()*_CAMERA_SPEED));
 				break;
 			case _BACK:
-				camera.position.z += delta()*_CAMERA_SPEED;
+				position.sub((new THREE.Vector3()).addScaledVector(direction,delta()*_CAMERA_SPEED));	
+				break;
+			case _LEFT:
+				position.sub((new THREE.Vector3()).addScaledVector(right,delta()*_CAMERA_SPEED));
+				break;
+			case _RIGHT:
+				position.add((new THREE.Vector3()).addScaledVector(right,delta()*_CAMERA_SPEED));
+				break;
+			case _UP:
+				position.add((new THREE.Vector3()).addScaledVector(up,delta()*_CAMERA_SPEED));
+				break;
+			case _DOWN:
+				position.sub((new THREE.Vector3()).addScaledVector(up,delta()*_CAMERA_SPEED));
 				break;
 		}
 	}
+	camera.position.x = position.x;
+	camera.position.y = position.y;
+	camera.position.z = position.z;
 	lastTimeUpdate = Date.now();
 }
--- WebGL/js/lights.js
+++ WebGL/js/lights.js
@@ -1,29 +1,22 @@
+// Constructor
 var Light = function() {
 	this.fogDensity = 0.45;
 	this.lightPos = new THREE.Vector3( 0, 100, 200 );
 	this.lightCol = new THREE.Vector3( 1, 0.5, 1 );
-	this.uniforms = {};
 };
 
 // Uniforms
 Light.prototype.getUniforms = function() {
-	this.addToUniforms(
+	return (
 	{
 		fogDensity: { type: "f", value: this.fogDensity },
 		lightPos: { type: "v3", value: this.lightPos },
 		lightCol: { type: "v3", value: this.lightCol }
 	}
 	);
-	return this.uniforms;
 };
 
-Light.prototype.setUniforms = function(uniforms) {
-	this.uniforms = uniforms;
-};
-Light.prototype.addToUniforms = function(moreUniforms) {
-	this.uniforms = $.extend({}, this.uniforms, moreUniforms);
-};
-
+// Setter
 Light.prototype.setColor = function(r, g, b ) {
 	this.lightCol.set(r, g, b );
 };
