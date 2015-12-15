8e793378bf6490ebfcb46c7345f8e7f7a86c57ff
Seehait Chockthanyawat <kiseehait@gmail.com>
2015-12-16 00:30:50 +0700

    Revert "Merge branch 'master' of https://github.com/kiseehait/realtime_final_project"
    This reverts commit c344535f15a7ad1a2fbf6483808e0969cf8020a3, reversing
    changes made to f7ef31f231de22912d6a4d58f621e9d5af452449.

--- WebGL/js/adder.js
+++ WebGL/js/adder.js
@@ -1,17 +1,3 @@
-function initAdder() {
-	var list = document.getElementById("name-list");
-	for (var i = 0; i < meshObject.length; i++) {
-		var element = document.createElement("option");
-		element.text = meshObject[i].o.objectName;
-		element.value = meshObject[i].o.objectName;
-		list.appendChild(element);
-	}
-}
-
-$( document ).ready(function() {
-	initAdder();
-});
-
 var addObject = function(name, type, mat, x, y, z, dx, dy, dz) {
 	var object = new MeshObject(name);
 	object.loadTexture(mat);
@@ -26,36 +12,7 @@ var addObject = function(name, type, mat, x, y, z, dx, dy, dz) {
 	object.mesh.receiveShadow = true;
 	scene.add(object.mesh);
 	meshObject.push({
-		o: object,
+		o: object.mesh,
 		rotFac: new THREE.Vector3( dx, dy, dz )
 	});
-
-	var list = document.getElementById("name-list");
-	var element = document.createElement("option");
-	element.text = name;
-	element.value = name;
-	list.appendChild(element);
 };
-
-var removeObject = function(name) {
-	meshObject.forEach(function(obj) {
-		if (obj.o.name == name) id = obj.o.id;
-	});
-	for(var i = meshObject.length - 1; i >= 0; i--) {
-		if(meshObject[i].o.objectName == name) {
-			scene.remove(meshObject[i].o.mesh);
-			meshObject.splice(i, 1);
-		}
-	}
-
-	var index = $("#name-list").get(0).selectedIndex;
-	var list = document.getElementById("name-list");
-	list.remove(index);
-};
-
-function addObjForm(name, type, mat, x, y, z, dx, dy, dz) {
-	dx = parseFloat(dx);
-	dy = parseFloat(dy);
-	dz = parseFloat(dz);
-    addObject(name, type, mat, x, y, z, dx, dy, dz);
-}
--- WebGL/js/main.js
+++ WebGL/js/main.js
@@ -52,7 +52,7 @@ cube1.mesh.castShadow = true;
 cube1.mesh.receiveShadow = true;
 scene.add(cube1.mesh);
 meshObject.push({
-	o: cube1,
+	o: cube1.mesh,
 	rotFac: new THREE.Vector3( 0.1, 0.1, 0.1 )
 });
 
@@ -69,7 +69,7 @@ cube2.mesh.castShadow = true;
 cube2.mesh.receiveShadow = true;
 scene.add(cube2.mesh);
 meshObject.push({
-	o: cube2,
+	o: cube2.mesh,
 	rotFac: new THREE.Vector3( 0.01, 0.01, 0.0 )
 });
 
@@ -88,11 +88,11 @@ tree1.mesh.receiveShadow = true;
 tree1.mesh.geometry.computeVertexNormals();
 scene.add(tree1.mesh);
 meshObject.push({
-	o: tree1,
+	o: tree1.mesh,
 	rotFac: new THREE.Vector3( 0.0, 0.05, 0.0 )
 });
 
-
+addObject("mycube", "cube.obj", "box.jpg", 10, 10, -10, 0.1, 0.0, 0.0);
 
 /*
 var ground = new MeshObject("ground");
@@ -178,10 +178,6 @@ Plane.mesh.position.y = 50;
 Plane.mesh.rotation.x = Math.PI/2;
 scene.add(Plane.mesh);
 
-/*
-removeObject("cube1");
-removeObject("tree1");
-*/
 
 var render = function () {
 	requestAnimationFrame( render );
@@ -201,9 +197,11 @@ var render = function () {
 	});
 
 	meshObject.forEach(function(obj) {
-		obj.o.mesh.rotation.x += obj.rotFac.x;
-		obj.o.mesh.rotation.y += obj.rotFac.y;
-		obj.o.mesh.rotation.z += obj.rotFac.z;
+		console.log(obj.o.id);
+
+		obj.o.rotation.x += obj.rotFac.x;
+		obj.o.rotation.y += obj.rotFac.y;
+		obj.o.rotation.z += obj.rotFac.z;
 	});
 };
 
